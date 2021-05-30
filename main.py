200c1a7540de29427a931468ce40a19e1f0609e0
Knei <>
2021-05-30 02:25:29 +0000

    Updated Link Help Command

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ async def help(ctx):
     brole = get(guild.roles, id=846825420993331203)
     Help = discord.Embed(title=f"{client.user.name}#{client.user.discriminator}'s Comand Help Page", description=f"**Bot Prefix**=`{prefix}`")
     Help.set_footer(text="Don't DM Knei#4714 with complaints")
-    Help.add_field(name="link",value= f"aliases: `['sync', 's', 'l']`\nCommand: `{prefix}link <IGN>`\nRoles Required: `Jr Carrier, Sr Carrier`")
+    Help.add_field(name="link",value= f"aliases: `['sync', 's', 'l']`\nCommand: `{prefix}link <IGN>`\nIf you need link help replace IGN with help\nRoles Required: `Jr Carrier, Sr Carrier`")
     if arole or brole in ctx.author.roles:
         Help.add_field(name="removeuser", value=f"aliases: `[ru, deleteuser, du]`\nCommand: `{prefix}removeuser <User>`\nRoles Required: `Carrier Manager, Staff Team`")
         Help.add_field(name="massremoveusers", value=f"aliases: `[mru, massdeleteuser, mdu]\nCommand: `{prefix}massremoveusers [users as mentions or IDs]`")
@@ -111,61 +111,71 @@ async def Update_Users():
     if Total !=0: print("Users Updated: {:2.1%}\nUsers Not Changed: {:2.1%}\nUsers Ignored/Not Found: {:2.1%}\nTotal Users: {}".format(UsersUp/Total, UsersNo/Total, UserUnknown/Total, Total))
     else: print("No users in database")
       
+hel = """```scala
+1. Type "/profile" in the in-game chat and press enter
+2. Find the icon called "Social Media"
+3. Find the icon called "Discord"
+4. Go to the Discord app and click on your name on the bottom left to copy your Discord tag (eg: Knei#4714[capitalization matters])
+5. Go back in game and paste that copied tag in the chat
+6. If a book pops up, click "I understand"
+```"""
 
 @client.command(aliases = ['l', 'link', 's'])
 @commands.has_any_role(843249027411607552, 843248725190508564, 719848521813196951)
 async def sync(ctx, User=None):
     con = MongoCon('users')
     if User != None:
-        UUID = MojangAPI.get_uuid(User)
-        User = MojangAPI.get_username(UUID)
-        for x in con.find():
-            if x['_id'] == ctx.author.id: 
-                await ctx.send('You are already linked!')
-                return
-            elif x['uuid'] == UUID: 
-                await ctx.send('This minecraft account is already linked!')
-                return
-            else: pass
-        if UUID != None:
-            j, s = await HypixelCon('player',uuid=UUID)
-            if s == 200:
-                try:    
-                    discord = j['player']['socialMedia']['links']['DISCORD']
-                except KeyError: 
-                    await ctx.send(User + " has no linked discord account.\nLink an account by going ingame\n ```scala\nPlayer Head > Social > Discord\n```")
-                if str(discord)==str(ctx.author):
-                    l = []
-                    j, s = await HypixelCon("skyblock/profiles", uuid = UUID)
-                    if s == 200:
-                        for x in j['profiles']: 
-                            try:    
-                                l.append(x['members'][f'{UUID}']['dungeons']['dungeon_types']["catacombs"]["experience"])
-                            except:
-                                pass
-                        level = await find(max(l))
-                        for x in j['profiles']:
+        if User.lower() != 'help':
+            UUID = MojangAPI.get_uuid(User)
+            User = MojangAPI.get_username(UUID)
+            for x in con.find():
+                if x['_id'] == ctx.author.id: 
+                    await ctx.send('You are already linked!')
+                    return
+                elif x['uuid'] == UUID: 
+                    await ctx.send('This minecraft account is already linked!')
+                    return
+                else: pass
+            if UUID != None:
+                j, s = await HypixelCon('player',uuid=UUID)
+                if s == 200:
+                    try:    
+                        discord = j['player']['socialMedia']['links']['DISCORD']
+                    except KeyError: 
+                        await ctx.send(User + " has no linked discord account.\n"+hel)
+                    if str(discord)==str(ctx.author):
+                        l = []
+                        j, s = await HypixelCon("skyblock/profiles", uuid = UUID)
+                        if s == 200:
+                            for x in j['profiles']: 
+                                try:    
+                                    l.append(x['members'][f'{UUID}']['dungeons']['dungeon_types']["catacombs"]["experience"])
+                                except:
+                                    pass
+                            level = await find(max(l))
+                            for x in j['profiles']:
+                                try:
+                                    if max(l) == x['members'][f'{UUID}']['dungeons']['dungeon_types']["catacombs"]["experience"]:
+                                        profile = x["profile_id"]
+                                except: pass
                             try:
-                                if max(l) == x['members'][f'{UUID}']['dungeons']['dungeon_types']["catacombs"]["experience"]:
-                                    profile = x["profile_id"]
-                            except: pass
-                        try:
-                            guild = client.get_guild(masterguild)
-                            role = get(guild.roles, id=AdminRole)
-                            if role not in ctx.author.roles:
-                                await ctx.author.edit(nick=f'[{level}] {User}')
-                                con.insert_one({"ign":User, "_id":ctx.author.id, "uuid":UUID ,"profile":profile})
-                                await ctx.send('Successfully Linked')   
-                            else: 
-                                con.insert_one({"ign":User, "_id":ctx.author.id, "uuid":UUID ,"profile":profile})
-                                await ctx.send('Successfully Linked')   
+                                guild = client.get_guild(masterguild)
+                                role = get(guild.roles, id=AdminRole)
+                                if role not in ctx.author.roles:
+                                    await ctx.author.edit(nick=f'[{level}] {User}')
+                                    con.insert_one({"ign":User, "_id":ctx.author.id, "uuid":UUID ,"profile":profile})
+                                    await ctx.send('Successfully Linked')   
+                                else: 
+                                    con.insert_one({"ign":User, "_id":ctx.author.id, "uuid":UUID ,"profile":profile})
+                                    await ctx.send('Successfully Linked')   
 
-    #Was lazy so I shoved all the fails down here
-                        except: await ctx.send('There was an error. Please Try again.')
-                    else: await ctx.send('Failed to connect to the skyblock profile endpoint.\nPlease try again.')
-                else: await ctx.send('The given discord does not match yours')  
-            else: await ctx.send('Failed to connect to Hypixel API please try again\nIf this happens multiple times the API might be down')
-        else: await ctx.send('Invalid username')
+        #Was lazy so I shoved all the fails down here
+                            except: await ctx.send('There was an error. Please Try again.')
+                        else: await ctx.send('Failed to connect to the skyblock profile endpoint.\nPlease try again.')
+                    else: await ctx.send('The given discord does not match yours')  
+                else: await ctx.send('Failed to connect to Hypixel API please try again\nIf this happens multiple times the API might be down')
+            else: await ctx.send('Invalid username')
+        else: await ctx.send(hel)
     else: await ctx.send('Please provide a username')
 
 
