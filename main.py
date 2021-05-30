2af4f804fc470dba1212cee1208a32536d18feb9
Knei <>
2021-05-30 02:07:10 +0000

    Added a mass purge command for quick deletion of users from database

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-import json
 import asyncio
 import aiohttp
 import discord
@@ -10,11 +9,13 @@ from discord.ext import commands, tasks
 
 
 # config (Update as you see fit)
-prefix = '$'
-client = commands.Bot(command_prefix=prefix, intents=discord.Intents.all(), case_insensitive = True) 
+prefix = '%'
+intents = discord.Intents.default()
+intents.members = True
+client = commands.Bot(command_prefix=prefix, intents=intents, case_insensitive = True)
 #please dont mess with ^^^^ thanks :)
 masterguild = int(os.environ["MGUILD"])
-
+AdminRole = 719848521813196951
 # Cata Update Frequency
 second = 0
 minute = 1
@@ -44,8 +45,7 @@ async def HypixelCon(endpoint, **kwargs):
 
 
 async def find(exp:int):
-    n = -1
-    j = MongoCon('config').find({"_id":"Dungeons"})
+    j, n = MongoCon('config').find_one({"_id":"Dungeons"}), 0
     for x in j['info']:
         if x['total'] > exp:
             return n
@@ -61,52 +61,58 @@ async def on_ready():
 
 @client.command() 
 async def help(ctx):
+    guild = client.get_guild(masterguild)
+    arole = get(guild.roles, id=AdminRole)
+    brole = get(guild.roles, id=846825420993331203)
     Help = discord.Embed(title=f"{client.user.name}#{client.user.discriminator}'s Comand Help Page", description=f"**Bot Prefix**=`{prefix}`")
     Help.set_footer(text="Don't DM Knei#4714 with complaints")
     Help.add_field(name="link",value= f"aliases: `['sync', 's', 'l']`\nCommand: `{prefix}link <IGN>`\nRoles Required: `Jr Carrier, Sr Carrier`")
+    if arole or brole in ctx.author.roles:
+        Help.add_field(name="removeuser", value=f"aliases: `[ru, deleteuser, du]`\nCommand: `{prefix}removeuser <User>`\nRoles Required: `Carrier Manager, Staff Team`")
     try:
         await ctx.author.send(embed=Help)
         await ctx.send("Check your DM's")
     except:
-        await ctx.send()
+        await ctx.send(embed=Help)
 
 
 @tasks.loop(seconds=second, minutes=minute, hours=hour)
 async def Update_Users():
     Total = UsersUp = UsersNo = UserUnknown = 0
     guild = client.get_guild(masterguild)
-    role = discord.utils.get(guild.roles, id=719848521813196951)#847470285330710538)
-    print("Updating...", end='\r')
+    role = discord.utils.get(guild.roles, id=AdminRole)
+    print("Updating...")
     for x in MongoCon('users').find():
         Total +=1
-        try:    user = guild.get_member(x["_id"])
-        except:     pass
+        try:    user = guild.get_member(int(x["_id"]))
+        except:    pass
         else:  
             try:
-                if role in user.roles: UserUnknown+=1
-                else:
-                    if user == None:  UsersNo+=1
-                    else: 
+                if role not in user.roles: 
+                    if user is not None:  
                         j, s = await HypixelCon("skyblock/profile", profile = x["profile"])
-                        if s != 200 or j["success"] != True: pass 
-                        else:
+                        if s == 200 and j["success"] == True: 
                             rank = await find(j['profile']['members'][f"{x['uuid']}"]['dungeons']['dungeon_types']["catacombs"]["experience"])
-                        if user.nick != None:   onick = user.nick
-                        else:   onick = None
+                        else: raise Exception
+                        if user.nick != None:   
+                            onick = user.nick
+                        else:   
+                            onick = None
                         nnick = f"[{rank}] {x['ign']}"
-                        if onick == nnick:  UsersNo+=1
-                        else: 
+                        if onick != nnick:
                             await user.edit(nick = nnick)
-                            UsersUp+=1
-            except: UserUnknown+=1
+                            UsersUp+=1  
+                        else:   UsersNo+=1 
+                    else:   UsersNo+=1
+                else:   UserUnknown+=1
+            except:     UserUnknown+=1
     print("Update Finished!")
-    if Total !=0:
-        print("\rUsers Updated: {:2.1%}\nUsers Not Changed: {:2.1%}\nNot Found: {:2.1%}\nTotal Users: {}".format(UsersUp/Total, UsersNo/Total, UserUnknown/Total, Total))
+    if Total !=0: print("Users Updated: {:2.1%}\nUsers Not Changed: {:2.1%}\nUsers Ignored/Not Found: {:2.1%}\nTotal Users: {}".format(UsersUp/Total, UsersNo/Total, UserUnknown/Total, Total))
     else: print("No users in database")
       
 
 @client.command(aliases = ['l', 'link', 's'])
-@commands.has_any_role(843249027411607552, 843248725190508564, 719848521813196951)#, 847470285330710538)
+@commands.has_any_role(843249027411607552, 843248725190508564, 719848521813196951)
 async def sync(ctx, User=None):
     con = MongoCon('users')
     if User != None:
@@ -119,6 +125,7 @@ async def sync(ctx, User=None):
             elif x['uuid'] == UUID: 
                 await ctx.send('This minecraft account is already linked!')
                 return
+            else: pass
         if UUID != None:
             j, s = await HypixelCon('player',uuid=UUID)
             if s == 200:
@@ -133,7 +140,8 @@ async def sync(ctx, User=None):
                         for x in j['profiles']: 
                             try:    
                                 l.append(x['members'][f'{UUID}']['dungeons']['dungeon_types']["catacombs"]["experience"])
-                            except:   pass
+                            except:
+                                pass
                         level = await find(max(l))
                         for x in j['profiles']:
                             try:
@@ -142,14 +150,16 @@ async def sync(ctx, User=None):
                             except: pass
                         try:
                             guild = client.get_guild(masterguild)
-                            role = get(guild.roles, id=719848521813196951)#847470285330710538)
+                            role = get(guild.roles, id=AdminRole)
                             if role not in ctx.author.roles:
                                 await ctx.author.edit(nick=f'[{level}] {User}')
-                            else:
+                                con.insert_one({"ign":User, "_id":ctx.author.id, "uuid":UUID ,"profile":profile})
+                                await ctx.send('Successfully Linked')   
+                            else: 
                                 con.insert_one({"ign":User, "_id":ctx.author.id, "uuid":UUID ,"profile":profile})
                                 await ctx.send('Successfully Linked')   
 
-#Was lazy so I shoved all the fails down here
+    #Was lazy so I shoved all the fails down here
                         except: await ctx.send('There was an error. Please Try again.')
                     else: await ctx.send('Failed to connect to the skyblock profile endpoint.\nPlease try again.')
                 else: await ctx.send('The given discord does not match yours')  
@@ -162,12 +172,11 @@ async def sync(ctx, User=None):
 async def on_sync_error(ctx, error):
     if isinstance(error, commands.errors.MissingAnyRole):
         await ctx.send("You don't have a carrier role!")
-    else:
-        pass
+    else: pass
 
 
 @client.command(aliases = ["ru", "deleteuser", "du"])
-@commands.has_any_role(719848521813196951, 719848521813196951)
+@commands.has_any_role(719848521813196951, 846825420993331203)
 async def removeuser(ctx, user: discord.Member):
     try:
         MongoCon('users').delete_one({"_id":user.id})
@@ -183,5 +192,29 @@ async def on_member_remove(member):
     except:
         await asyncio.sleep(60)
         MongoCon('users').delete_one({"_id":member.id})
+
+
+@client.command(aliases = ["mru", "massdeleteuser", "mdu"])
+@commands.has_any_role(719848521813196951, 846825420993331203)
+async def massremoveusers(ctx, *user: discord.Member):
+    Removed = NotFound = 0
+    l = []
+    con = MongoCon('users')
+    msg = await ctx.send("Deleting Users...")
+    for x in user:
+        for y in con.find():
+            if y["_id"] == x.id:
+                con.delete_one(x)
+                Removed +=1 
+        l.append(x)
+        NotFound +=1
+    if NotFound != 0:
+        message = f"\nRemoved {Removed} users with {NotFound} users skipped due to not being found||(And maybe an error)||.\nUsers Skipped:\n"
+        for x in l:
+            message += f"{x.mention}[{x.id}], "
+    else:
+        message = f"\nAll {Removed} users removed!"
+    await msg.edit(content=f"Purge Completed!{message[:-2]}")
+
 
 client.run(os.environ["Carrier"])
