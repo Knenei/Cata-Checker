0687787d1234a9741bc1e12a613a754d72aa7e7d
Knei <76569087+notKnei@users.noreply.github.com>
2021-07-04 17:14:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ from mojang import MojangAPI
 from discord.utils import get
 from discord.ext import commands, tasks
 from datetime import datetime, timedelta
-from ratelimit import limits, sleep_and_retry
+#from ratelimit import limits, sleep_and_retry
 
 
-#Version 1.3.0
+#Version 1.6.0
 owner = 418531067008647169
 # config (Update as you see fit)
 prefix = "%"
@@ -26,7 +26,196 @@ second = 0
 minute = 5
 hour = 0
 
-#  Code
+
+
+# =================================================================================
+#
+#  THIS CODE IS IN NO WAY MY PROPERTY AND IS A EDITED VERSION OF THE ORIGINIAL TO ALLOW ASYNC SLEEP INSEAD OF SYNC SLEEP
+#
+# =================================================================================
+'''
+Rate limit public interface.
+
+This module includes the decorator used to rate limit function invocations.
+Additionally this module includes a naive retry strategy to be used in
+conjunction with the rate limit decorator.
+'''
+from functools import wraps
+from math import floor
+
+import time
+import sys
+import threading
+
+import time
+
+def now():
+    '''
+    Use monotonic time if available, otherwise fall back to the system clock.
+
+    :return: Time function.
+    :rtype: function
+    '''
+    if hasattr(time, 'monotonic'):
+        return time.monotonic
+    return time.time
+
+
+class RateLimitException(Exception):
+    '''
+    Rate limit exception class.
+    '''
+    def __init__(self, message, period_remaining):
+        '''
+        Custom exception raise when the number of function invocations exceeds
+        that imposed by a rate limit. Additionally the exception is aware of
+        the remaining time period after which the rate limit is reset.
+
+        :param string message: Custom exception message.
+        :param float period_remaining: The time remaining until the rate limit is reset.
+        '''
+        super(RateLimitException, self).__init__(message)
+        self.period_remaining = period_remaining
+
+
+class RateLimitDecorator(object):
+    '''
+    Rate limit decorator class.
+    '''
+    def __init__(self, calls=15, period=900, clock=now(), raise_on_limit=True):
+        '''
+        Instantiate a RateLimitDecorator with some sensible defaults. By
+        default the Twitter rate limiting window is respected (15 calls every
+        15 minutes).
+
+        :param int calls: Maximum function invocations allowed within a time period.
+        :param float period: An upper bound time period (in seconds) before the rate limit resets.
+        :param function clock: An optional function retuning the current time.
+        :param bool raise_on_limit: A boolean allowing the caller to avoiding rasing an exception.
+        '''
+        self.clamped_calls = max(1, min(sys.maxsize, floor(calls)))
+        self.period = period
+        self.clock = clock
+        self.raise_on_limit = raise_on_limit
+
+        # Initialise the decorator state.
+        self.last_reset = clock()
+        self.num_calls = 0
+
+        # Add thread safety.
+        self.lock = threading.RLock()
+
+    def __call__(self, func):
+        '''
+        Return a wrapped function that prevents further function invocations if
+        previously called within a specified period of time.
+
+        :param function func: The function to decorate.
+        :return: Decorated function.
+        :rtype: function
+        '''
+        @wraps(func)
+        def wrapper(*args, **kargs):
+            '''
+            Extend the behaviour of the decorated function, forwarding function
+            invocations previously called no sooner than a specified period of
+            time. The decorator will raise an exception if the function cannot
+            be called so the caller may implement a retry strategy such as an
+            exponential backoff.
+
+            :param args: non-keyword variable length argument list to the decorated function.
+            :param kargs: keyworded variable length argument list to the decorated function.
+            :raises: RateLimitException
+            '''
+            with self.lock:
+                period_remaining = self.__period_remaining()
+
+                # If the time window has elapsed then reset.
+                if period_remaining <= 0:
+                    self.num_calls = 0
+                    self.last_reset = self.clock()
+
+                # Increase the number of attempts to call the function.
+                self.num_calls += 1
+
+                # If the number of attempts to call the function exceeds the
+                # maximum then raise an exception.
+                if self.num_calls > self.clamped_calls:
+                    if self.raise_on_limit:
+                        raise RateLimitException('too many calls', period_remaining)
+                    return
+
+            return func(*args, **kargs)
+        return wrapper
+
+    def __period_remaining(self):
+        '''
+        Return the period remaining for the current rate limit window.
+
+        :return: The remaing period.
+        :rtype: float
+        '''
+        elapsed = self.clock() - self.last_reset
+        return self.period - elapsed
+
+async def AsyncSleep( time ):
+    await asyncio.sleep( time )
+
+def sleep_and_retry(func):
+    '''
+    Return a wrapped function that rescues rate limit exceptions, sleeping the
+    current thread until rate limit resets.
+
+    :param function func: The function to decorate.
+    :return: Decorated function.
+    :rtype: function
+    '''
+    @wraps(func)
+    def wrapper(*args, **kargs):
+        '''
+        Call the rate limited function. If the function raises a rate limit
+        exception sleep for the remaing time period and retry the function.
+
+        :param args: non-keyword variable length argument list to the decorated function.
+        :param kargs: keyworded variable length argument list to the decorated function.
+        '''
+        while True:
+            try:
+                return func(*args, **kargs)
+            except RateLimitException as exception:
+                #time.sleep(exception.period_remaining)
+                asyncio.run( AsyncSleep( exception.period_remaining ) )
+                
+    return wrapper
+
+# =================================================================================
+#
+#  THIS CODE IS IN NO WAY MY PROPERTY AND IS A EDITED VERSION OF THE ORIGINIAL TO ALLOW ASYNC SLEEP INSEAD OF SYNC SLEEP
+#
+# ==============================================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ===============================================================================================================
+#  Actual Code Below
+# ===============================================================================================================
 client.remove_command("help")
 
 def MongoCon(typ):
@@ -110,8 +299,8 @@ async def Update_Users( ):
       if discord in AC and discord not in ST:
         j, s = HypixelConnection( "skyblock/profile", profile = profile[ "profile" ])
         if s != 204 and s < 400:
-          Level = find( j[ "profile" ][ "members" ][ profile[ "uuid" ] ][ "dungeons" ][ "dungeon_types" ] )
-          if not discord.nick or discord.nick != f"[{ Level }] { j[ 'ign' ]}":
+          Level = find( j[ "profile" ][ "members" ][ profile[ "uuid" ] ][ "dungeons" ][ "dungeon_types" ][ "catacombs" ][ "experience" ] )
+          if not discord.nick or discord.nick != f"[{ Level }] { profile[ 'ign' ]}":
             await discord.edit( nick = f"[{ Level }] { profile[ 'ign' ] }" )
             UsersUp += 1
             if discord not in SC + MC + UC and Level == 32:
