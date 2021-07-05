ca9a7b02ea08ef7657e446b215d96197144557fd
Knei <76569087+notKnei@users.noreply.github.com>
2021-07-05 15:42:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import asyncio
 import discord
 import pymongo
 import requests
+from ratelimit import limits, RateLimitException
 from mojang import MojangAPI
 from discord.utils import get
 from discord.ext import commands, tasks
 from datetime import datetime, timedelta
-#from ratelimit import limits, sleep_and_retry
 
 
 #Version 1.6.0
@@ -23,199 +23,13 @@ SSB = 715801930877894706
 StaffTeam = 719848521813196951
 # Cata Update Frequency
 second = 0
-minute = 5
+minute = 10
 hour = 0
-
-
-
-# =================================================================================
-#
-#  THIS CODE IS IN NO WAY MY PROPERTY AND IS A EDITED VERSION OF THE ORIGINIAL TO ALLOW ASYNC SLEEP INSEAD OF SYNC SLEEP
-#
-# =================================================================================
-'''
-Rate limit public interface.
-
-This module includes the decorator used to rate limit function invocations.
-Additionally this module includes a naive retry strategy to be used in
-conjunction with the rate limit decorator.
-'''
-from functools import wraps
-from math import floor
-
-import time
-import sys
-import threading
-
-import time
-
-def now():
-    '''
-    Use monotonic time if available, otherwise fall back to the system clock.
-
-    :return: Time function.
-    :rtype: function
-    '''
-    if hasattr(time, 'monotonic'):
-        return time.monotonic
-    return time.time
-
-
-class RateLimitException(Exception):
-    '''
-    Rate limit exception class.
-    '''
-    def __init__(self, message, period_remaining):
-        '''
-        Custom exception raise when the number of function invocations exceeds
-        that imposed by a rate limit. Additionally the exception is aware of
-        the remaining time period after which the rate limit is reset.
-
-        :param string message: Custom exception message.
-        :param float period_remaining: The time remaining until the rate limit is reset.
-        '''
-        super(RateLimitException, self).__init__(message)
-        self.period_remaining = period_remaining
-
-
-class RateLimitDecorator(object):
-    '''
-    Rate limit decorator class.
-    '''
-    def __init__(self, calls=15, period=900, clock=now(), raise_on_limit=True):
-        '''
-        Instantiate a RateLimitDecorator with some sensible defaults. By
-        default the Twitter rate limiting window is respected (15 calls every
-        15 minutes).
-
-        :param int calls: Maximum function invocations allowed within a time period.
-        :param float period: An upper bound time period (in seconds) before the rate limit resets.
-        :param function clock: An optional function retuning the current time.
-        :param bool raise_on_limit: A boolean allowing the caller to avoiding rasing an exception.
-        '''
-        self.clamped_calls = max(1, min(sys.maxsize, floor(calls)))
-        self.period = period
-        self.clock = clock
-        self.raise_on_limit = raise_on_limit
-
-        # Initialise the decorator state.
-        self.last_reset = clock()
-        self.num_calls = 0
-
-        # Add thread safety.
-        self.lock = threading.RLock()
-
-    def __call__(self, func):
-        '''
-        Return a wrapped function that prevents further function invocations if
-        previously called within a specified period of time.
-
-        :param function func: The function to decorate.
-        :return: Decorated function.
-        :rtype: function
-        '''
-        @wraps(func)
-        def wrapper(*args, **kargs):
-            '''
-            Extend the behaviour of the decorated function, forwarding function
-            invocations previously called no sooner than a specified period of
-            time. The decorator will raise an exception if the function cannot
-            be called so the caller may implement a retry strategy such as an
-            exponential backoff.
-
-            :param args: non-keyword variable length argument list to the decorated function.
-            :param kargs: keyworded variable length argument list to the decorated function.
-            :raises: RateLimitException
-            '''
-            with self.lock:
-                period_remaining = self.__period_remaining()
-
-                # If the time window has elapsed then reset.
-                if period_remaining <= 0:
-                    self.num_calls = 0
-                    self.last_reset = self.clock()
-
-                # Increase the number of attempts to call the function.
-                self.num_calls += 1
-
-                # If the number of attempts to call the function exceeds the
-                # maximum then raise an exception.
-                if self.num_calls > self.clamped_calls:
-                    if self.raise_on_limit:
-                        raise RateLimitException('too many calls', period_remaining)
-                    return
-
-            return func(*args, **kargs)
-        return wrapper
-
-    def __period_remaining(self):
-        '''
-        Return the period remaining for the current rate limit window.
-
-        :return: The remaing period.
-        :rtype: float
-        '''
-        elapsed = self.clock() - self.last_reset
-        return self.period - elapsed
-
-async def AsyncSleep( time ):
-    await asyncio.sleep( time )
-
-def sleep_and_retry(func):
-    '''
-    Return a wrapped function that rescues rate limit exceptions, sleeping the
-    current thread until rate limit resets.
-
-    :param function func: The function to decorate.
-    :return: Decorated function.
-    :rtype: function
-    '''
-    @wraps(func)
-    def wrapper(*args, **kargs):
-        '''
-        Call the rate limited function. If the function raises a rate limit
-        exception sleep for the remaing time period and retry the function.
-
-        :param args: non-keyword variable length argument list to the decorated function.
-        :param kargs: keyworded variable length argument list to the decorated function.
-        '''
-        while True:
-            try:
-                return func(*args, **kargs)
-            except RateLimitException as exception:
-                #time.sleep(exception.period_remaining)
-                asyncio.run( AsyncSleep( exception.period_remaining ) )
-                
-    return wrapper
-
-# =================================================================================
-#
-#  THIS CODE IS IN NO WAY MY PROPERTY AND IS A EDITED VERSION OF THE ORIGINIAL TO ALLOW ASYNC SLEEP INSEAD OF SYNC SLEEP
-#
-# ==============================================================================
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 # ===============================================================================================================
 #  Actual Code Below
 # ===============================================================================================================
+
 client.remove_command("help")
 
 def MongoCon(typ):
@@ -225,17 +39,16 @@ def MongoCon(typ):
   elif typ == "config":
     return client.discord.config
 
-
-
-@sleep_and_retry
-@limits( calls = 100, period = 61 )
+@limits( calls = 120, period = 60 )
 def HypixelConnection( endpoint, **kwargs ):
   ot = ""
   if kwargs:
     for k, v in kwargs.items( ):
       ot += f"&{ k }={ v }"
   r = requests.get("https://api.hypixel.net/"+endpoint+"?key="+os.environ["HYPY"]+ot)
-  return r.json(), r.status_code
+  json = r.json()
+  status = r.status_code
+  return [json, status]
 
 
 
@@ -297,13 +110,17 @@ async def Update_Users( ):
     if discord:
       Total += 1
       if discord in AC and discord not in ST:
-        j, s = HypixelConnection( "skyblock/profile", profile = profile[ "profile" ])
+        try:
+          j, s = HypixelConnection( "skyblock/profile", profile = profile[ "profile" ])
+        except RateLimitException as exception:
+          await asyncio.sleep( exception.period_remaining )
+          j, s = HypixelConnection( "skyblock/profile", profile = profile[ "profile" ])
         if s != 204 and s < 400:
           Level = find( j[ "profile" ][ "members" ][ profile[ "uuid" ] ][ "dungeons" ][ "dungeon_types" ][ "catacombs" ][ "experience" ] )
           if not discord.nick or discord.nick != f"[{ Level }] { profile[ 'ign' ]}":
             await discord.edit( nick = f"[{ Level }] { profile[ 'ign' ] }" )
             UsersUp += 1
-            if discord not in SC + MC + UC and Level == 32:
+            if discord not in SC.members + MC.members + UC.members and Level == 32:
               await discord.add_roles( roles = [ SC ], reason = "Meets requirements for Senior Carrier")
               await discord.remove_roles( roles = [ JC ], reason = "Met requirements for Senior Carrier")
           else: 
@@ -317,40 +134,6 @@ async def Update_Users( ):
 
   print( "Update has finished!" )
   print("Users Updated: {:2.1%}\nUsers Not Changed: {:2.1%}\nUsers Ignored/Not Found: {:2.1%}\nTotal Users: {}".format(UsersUp/Total, UsersNo/Total, UserUnknown/Total, Total))
-
-#  print("Updating...")
-#  for x in MongoCon("users").find():
-#    Total +=1
-#    try:  user = guild.get_member(int(x["_id"]))
-#    except:  pass
-#    else:  
-#      try:
-#        if user is not None:  
-#          if role in user.roles: 
-#            UserUnknown+=1
-#          elif Sr in user.roles or Jr in user.roles:
-#            j, s = await HypixelCon("skyblock/profile", profile = x["profile"])
-#            if s == 200 and j["success"] == True: 
-#              rank = await find(j["profile"]["members"][x["uuid"]]["dungeons"]["dungeon_types"]["catacombs"]["experience"])
-#            else: raise Exception
-#            if user.nick != None:   
-#              onick = user.nick
-#            else:   
-#              onick = None
-#            nnick = f"[{rank}] {x['ign']}"
-#            if onick != nnick:
-#              await user.edit(nick = nnick)
-#              UsersUp+=1  
-#            else:   UsersNo+=1 
-#            if Sr not in user.roles and int(rank) >= 32:
-#              await user.add_roles(roles=Sr,reason="Meet catacombs requirments")
-#              await user.remove_roles(roles=Jr,reason="Has Senior Carrier")
-#          else:   UsersNo+=1
-#        else:   UserUnknown+=1
-#      except:   UserUnknown+=1
-#  print("Update Finished!")
-#  if Total !=0: print("Users Updated: {:2.1%}\nUsers Not Changed: {:2.1%}\nUsers Ignored/Not Found: {:2.1%}\nTotal Users: {}".format(UsersUp/Total, UsersNo/Total, UserUnknown/Total, Total))
-#  else: print("No users in database")
 
 
 @Update_Users.error
@@ -392,9 +175,9 @@ async def link( ctx, User = None ):
     return await ctx.send( "Please provide a username!" )
   if User.lower() == 'help':
     return await ctx.send( hel )
-  if con.find_one( { "_id" : ctx.author.id }.count( ) ) > 0:
+  if con.count_documents( { '_id': ctx.author.id }, limit = 1 ) != 0:
     return await ctx.send( "You are already linked to " + str(con.find( { "_id" : ctx.author.id } )[ "ign" ] ) )
-  if con.find_one( { "ign" : User } ).count( ) > 0:
+  if con.count_documents( { 'ign': User }, limit = 1 ) != 0:
     return await ctx.send( "This Username is already linked to another user!" )
   UUID = MojangAPI.get_uuid( User )
   if UUID:
@@ -404,14 +187,17 @@ async def link( ctx, User = None ):
         Discord = j1[ "player" ][ "socialMedia" ][ "links" ][ "DISCORD" ]
       except: 
         return await ctx.send( f"{ User } as no linked Discord Accounts\n" + hel )
-      if Discord == ctx.author:
-        j2, s2 = HypixelConnection( "skyblock/profiles", uuid = UUID )
-        if s2 != 200 and s2 < 400:
+      if str( Discord ) == str( ctx.author ):
+        j2, s2 = HypixelConnection( "skyblock/profiles", uuid = UUID ) 
+        if s2 != 204 and s2 < 400:
           Profile, CataExp = "", 0
           for x in j2[ "profiles" ]:
-            if x[ "members" ][ UUID ][ "dungeons"]["dungeon_types"]["catacombs"]["experience"] > CataExp:
-              CataExp = x[ "members" ][ UUID ][ "dungeons"]["dungeon_types"]["catacombs"]["experience"]
-              Profile = x
+            try:
+              if x[ "members" ][ UUID ][ "dungeons"]["dungeon_types"]["catacombs"]["experience"] > CataExp:
+                CataExp = x[ "members" ][ UUID ][ "dungeons"]["dungeon_types"]["catacombs"]["experience"]
+                Profile = x
+            except KeyError:
+              pass
           CataLevel = find( CataExp )
           if ctx.author not in ST:
             await ctx.author.edit( nick = f"[{ CataLevel }] { User }")
@@ -426,103 +212,52 @@ async def link( ctx, User = None ):
   else:
     return await ctx.send( f"{ User } is not a valid Minecraft Account!" )
   
-#  if User.lower() != "help":
-#    if User != None:
-#      UUID = MojangAPI.get_uuid(User)
-#      User = MojangAPI.get_username(UUID)
-#      for x in con.find():
-#        if x["_id"] == ctx.author.id:
-#          if Jr or Sr in ctx.author.roles:
-#            await ctx.send("You are already linked!")
-#            return
-#        elif x["uuid"] == UUID: 
-#          await ctx.send("This minecraft account is already linked!")
-#          return
-#      if UUID != None:
-#        j, s = await HypixelCon("player",uuid=UUID)
-#        if s == 200:
-#          try:  
-#            discord = j["player"]["socialMedia"]["links"]["DISCORD"]
-#          except KeyError: 
-#            await ctx.send(User + " has no linked discord account.\n"+hel)
-#          if str(discord)==str(ctx.author):
-#            l = []
-#            j, s = await HypixelCon("skyblock/profiles", uuid = UUID)
-#            if s == 200:
-#              for x in j["profiles"]: 
-#                try:  
-#                  l.append(x["members"][f"{UUID}"]["dungeons"]["dungeon_types"]["catacombs"]["experience"])
-#                except:
-#                  pass
-#              level = await find(max(l))
-#              for x in j["profiles"]:
-#                try:
-#                  if max(l) == x["members"][f"{UUID}"]["dungeons"]["dungeon_types"]["catacombs"]["experience"]:
-#                    profile = x["profile_id"]
-#                except: pass
-#              try:
-#                if role not in ctx.author.roles:
-#                  await ctx.author.edit(nick=f"[{level}] {User}")
-#                  con.insert_one({"ign":User, "_id":ctx.author.id, "uuid":UUID ,"profile":profile})
-#                  await ctx.send("Successfully Linked")   
-#                else: 
-#                  con.insert_one({"ign":User, "_id":ctx.author.id, "uuid":UUID ,"profile":profile})
-#                  await ctx.send("Successfully Linked")   
-#
-#    #Was lazy so I shoved all the fails down here
-#              except: await ctx.send("There was an error. Please Try again.")
-#            else: await ctx.send("Failed to connect to the skyblock profile endpoint.\nPlease try again.")
-#          else: await ctx.send("The given discord does not match yours")  
-#        else: await ctx.send("Failed to connect to Hypixel API please try again\nIf this happens multiple times the API might be down")
-#      else: await ctx.send("Invalid username")
-#    else: await ctx.send("Please provide a username")
-#  else: await ctx.send(hel)
 
-@client.command(aliases = ["ru", "deleteuser", "du"])
-@commands.has_any_role(719848521813196951, 846825420993331203)
-async def removeuser(ctx, user: discord.Member):
+@client.command( aliases = [ "ru", "deleteuser", "du" ] )
+@commands.has_any_role( 719848521813196951, 846825420993331203 )
+async def removeuser( ctx, user: discord.Member ):
   try:
-    MongoCon("users").delete_one({"_id":user.id})
-    await ctx.send("User Removed!")
+    MongoCon( "users" ).delete_one( { "_id":user.id } )
+    await ctx.send( "User Removed!" )
   except:
-    await ctx.send("User Not Found")
+    await ctx.send( "User Not Found" )
 
 
 @client.event 
-async def on_member_remove(member):
+async def on_member_remove( member ):
   try:
-    MongoCon("users").delete_one({"_id":member.id})
+    MongoCon( "users" ).delete_one( { "_id":member.id } )
   except:
-    await asyncio.sleep(60)
-    MongoCon("users").delete_one({"_id":member.id})
+    await asyncio.sleep( 60 )
+    MongoCon( "users" ).delete_one( { "_id":member.id } )
 
 
-@client.command(aliases = ["mru", "massdeleteuser", "mdu"])
-@commands.has_any_role(719848521813196951, 846825420993331203)
-async def massremoveusers(ctx, *user: discord.Member):
+@client.command( aliases = [ "mru", "massdeleteuser", "mdu" ] )
+@commands.has_any_role( 719848521813196951, 846825420993331203 )
+async def massremoveusers( ctx, *user: discord.Member ):
   Removed = NotFound = 0
-  l = []
-  con = MongoCon("users")
-  msg = await ctx.send("Deleting Users...")
+  l = [ ]
+  con = MongoCon( "users" )
+  msg = await ctx.send( "Deleting Users..." )
   for x in user:
-    for y in con.find():
-      if y["_id"] == x.id:
-        con.delete_one(x)
+    for y in con.find( ):
+      if y[ "_id" ] == x.id:
+        con.delete_one( x )
         Removed +=1 
-    l.append(x)
+    l.append( x )
     NotFound +=1
   if NotFound != 0:
-    message = f"\nRemoved {Removed} users with {NotFound} users skipped due to not being found||(And maybe an error)||.\nUsers Skipped:\n"
+    message = f"\nRemoved { Removed } users with { NotFound } users skipped due to not being found||(And maybe an error)||.\nUsers Skipped:\n"
     for x in l:
-      message += f"{x.mention}[{x.id}], "
+      message += f"{ x.mention }[{ x.id }], "
   else:
-    message = f"\nAll {Removed} users removed!"
-  await msg.edit(content=f"Purge Completed!{message[:-2]}")
+    message = f"\nAll { Removed } users removed!"
+  await msg.edit( content = f"Purge Completed!{ message[ :-2 ] }" )
 
 
-@client.command(aliases = ["sc"])
-async def ScamCheck(ctx, IGN=None):
-  await ctx.send("Deprecated For Now.")
+@client.command( aliases = [ "sc" ] )
+async def ScamCheck( ctx, IGN=None ):
+  await ctx.send( "Deprecated For Now." )
   #if IGN != None:
   #  uuid = MojangAPI.get_uuid(IGN)
   #  if uuid != None:
@@ -555,21 +290,21 @@ async def ScamCheck(ctx, IGN=None):
 #  await ctx.send("You aren't linked!")
 
 
-@client.command()
-@commands.has_any_role(719848521813196951, 846825420993331203)
-async def runThrough(ctx):
-  con = MongoCon("users")
-  guild = client.get_guild(SSB)
-  mod = get(guild.roles, id=StaffTeam).members
-  Jr = get(guild.roles, id=843249027411607552).members
-  Sr = get(guild.roles, id=843248725190508564).members
-  li = [Jr, Sr]
+@client.command( )
+@commands.has_any_role( 719848521813196951, 846825420993331203 )
+async def runThrough( ctx ):
+  con = MongoCon( "users" )
+  guild = client.get_guild( SSB )
+  mod = get( guild.roles, id=StaffTeam ).members
+  Jr = get( guild.roles, id=843249027411607552 ).members
+  Sr = get( guild.roles, id=843248725190508564 ).members
+  li = [ Jr, Sr ]
   l, strs = [], ""
-  msg = await ctx.send("Searching...")
+  msg = await ctx.send( "Searching..." )
   for z in li:
     for y in z:
-      r=con.find_one({"_id":y.id})
-      if type(r) != dict:
+      r=con.find_one( { "_id":y.id } )
+      if type( r ) != dict:
         if y not in mod:
             l.append( y.id )
   msg1 = await ctx.send( "Missing Users:" )
